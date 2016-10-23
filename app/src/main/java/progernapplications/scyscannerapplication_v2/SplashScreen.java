8c6712b2fb560a40d900993a67916a0860cc2466
Oleg Misko <olegmiskoo@gmail.com>
2016-10-23 18:01:40 +0300

    Added loading animation view on splash screen. Created Main Drawer Activity. Redesigned menu items in MDA. Deleted AppBar Menu. Redesigned Drawer. Added drawable icons.

--- app/src/main/java/progernapplications/scyscannerapplication_v2/SplashScreen.java
+++ app/src/main/java/progernapplications/scyscannerapplication_v2/SplashScreen.java
@@ -1,13 +1,34 @@
 package progernapplications.scyscannerapplication_v2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+import com.eyalbira.loadingdots.LoadingDots;
+
+public class SplashScreen extends Activity {
+
+    private Handler mTimeHandler;
+    private Intent startMainActivity;
+    private LoadingDots mLoadingDots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        startMainActivity = new Intent(this, MainActivity.class);
+        mLoadingDots = (LoadingDots) findViewById(R.id.splash_dots);
+
+        // Start Main Activity after some time
+
+        mTimeHandler = new Handler();
+        mTimeHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mLoadingDots.stopAnimation();
+                startActivity(startMainActivity);
+            }
+        }, 4200);
     }
 }
