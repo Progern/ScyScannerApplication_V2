2876991306136c09f3a374c2285b3912d323b7da
Oleg Misko <olegmiskoo@gmail.com>
2016-10-26 18:04:52 +0300

    - HOTFIX: Fixed not working request (locale request for now)

--- app/src/main/java/progernapplications/scyscannerapplication_v2/Locale.java
+++ app/src/main/java/progernapplications/scyscannerapplication_v2/Locale.java
@@ -1,8 +1,14 @@
 package progernapplications.scyscannerapplication_v2;
 
 
+import com.google.gson.annotations.SerializedName;
+
 public class Locale {
+
+    @SerializedName("Code")
     private String code;
+
+    @SerializedName("Name")
     private String name;
 
     public Locale(String code, String name) {
@@ -15,6 +21,11 @@ public class Locale {
     }
 
     public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
         return name;
     }
 }
--- app/src/main/java/progernapplications/scyscannerapplication_v2/SettingsFragment.java
+++ app/src/main/java/progernapplications/scyscannerapplication_v2/SettingsFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -24,23 +25,23 @@ import cz.msebera.android.httpclient.Header;
 
 public class SettingsFragment extends Fragment implements View.OnClickListener {
 
-    private static final String LOCALE_REQUEST = "http://partners.api.skyscanner.net/apiservices/reference/v1.0/locales?apiKey=zd325407587237928226567311928563";
-    private static final String CURRENCIES_REQUEST = "http://partners.api.skyscanner.net/apiservices/reference/v1.0/currencies?apiKey=zd325407587237928226567311928563";
+    private static final String LOCALE_REQUEST = "http://partners.api.skyscanner.net/apiservices/reference/v1.0/locales?apiKey=zd091739373958921124481496042447";
+    private static final String CURRENCIES_REQUEST = "http://partners.api.skyscanner.net/apiservices/reference/v1.0/currencies?apiKey=zd091739373958921124481496042447";
 
 
-    private AutoCompleteTextView localesText, currensiesText, countriesText;
-    private String resultLocale, resultCurrency, resultContry;
+    private AutoCompleteTextView localesText; //, currensiesText, countriesText;
 
     private AsyncHttpClient mClient;
     private RequestParams params;
 
     // Array Lists for handling our responses
-    private ArrayList<MarketCountry> countriesResponseList;
-    private ArrayList<Currency> currenciesResponseList;
+    //private ArrayList<MarketCountry> countriesResponseList;
+    //private ArrayList<Currency> currenciesResponseList;
     private ArrayList<Locale> localesResponseList;
 
-    private ArrayAdapter<MarketCountry> marketCountryArrayAdapter;
-    private ArrayAdapter<Currency> currencyArrayAdapter;
+
+    //private ArrayAdapter<MarketCountry> marketCountryArrayAdapter;
+    //private ArrayAdapter<Currency> currencyArrayAdapter;
     private ArrayAdapter<Locale> localeArrayAdapter;
 
 
@@ -49,29 +50,31 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_settings, container, false);
         localesText = (AutoCompleteTextView) myView.findViewById(R.id.locale_autocomp);
-        currensiesText = (AutoCompleteTextView) myView.findViewById(R.id.currency_autocomp);
-        countriesText = (AutoCompleteTextView) myView.findViewById(R.id.marketCountry_autocomp);
+        //currensiesText = (AutoCompleteTextView) myView.findViewById(R.id.currency_autocomp);
+        //countriesText = (AutoCompleteTextView) myView.findViewById(R.id.marketCountry_autocomp);
 
         mClient = new AsyncHttpClient();
         params = new RequestParams();
         params.add("Accept", "application/json");
 
-        countriesResponseList = new ArrayList<>();
-        currenciesResponseList = new ArrayList<>();
+        //countriesResponseList = new ArrayList<>();
+        //currenciesResponseList = new ArrayList<>();
         localesResponseList = new ArrayList<>();
 
 
-        marketCountryArrayAdapter = new ArrayAdapter<MarketCountry>(getContext(), R.layout.support_simple_spinner_dropdown_item, countriesResponseList);
-        currencyArrayAdapter = new ArrayAdapter<Currency>(getContext(), R.layout.support_simple_spinner_dropdown_item, currenciesResponseList);
-        localeArrayAdapter = new ArrayAdapter<>(getContext(), R.layout.support_simple_spinner_dropdown_item, localesResponseList);
+        //marketCountryArrayAdapter = new ArrayAdapter<MarketCountry>(getContext(), R.layout.spinner_item_layout, countriesResponseList);
+        //currencyArrayAdapter = new ArrayAdapter<Currency>(getContext(), R.layout.spinner_item_layout, currenciesResponseList);
+        localeArrayAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_item_layout, localesResponseList);
 
         localesText.setOnClickListener(this);
-        currensiesText.setOnClickListener(this);
-        countriesText.setOnClickListener(this);
+        //currensiesText.setOnClickListener(this);
+        //countriesText.setOnClickListener(this);
 
         localesText.setAdapter(localeArrayAdapter);
-        currensiesText.setAdapter(currencyArrayAdapter);
-        countriesText.setAdapter(marketCountryArrayAdapter);
+        getLocale();
+
+        //currensiesText.setAdapter(currencyArrayAdapter);
+        //countriesText.setAdapter(marketCountryArrayAdapter);
 
 
         return myView;
@@ -81,7 +84,8 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.locale_autocomp:
-                getLocale();
+                Toast.makeText(getContext(), "Test toast for click", Toast.LENGTH_SHORT).show();
+
                 break;
             case R.id.currency_autocomp:
                 // TODO
@@ -93,19 +97,21 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
     }
 
 
-    // TODO BD cashing later!
+    // TODO - - - Cashing results in BD once and always - - -
     private void getLocale() {
         mClient.get(LOCALE_REQUEST, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 if (response != null) {
                     try {
+                        Toast.makeText(getActivity().getApplicationContext(), "NOT NULL", Toast.LENGTH_SHORT).show();
                         JSONArray locales = response.getJSONArray("Locales");
+                        if (locales == null || response == null)
+                            Toast.makeText(getActivity().getApplicationContext(), "NULL", Toast.LENGTH_SHORT).show();
 
                         for (int i = 0; i < locales.length(); i++) {
                             localesResponseList.add(new Locale(locales.getJSONObject(i).getString("Code"), locales.getJSONObject(i).getString("Name")));
                         }
-
                         localeArrayAdapter.notifyDataSetChanged();
                     } catch (JSONException e) {
 
