82cb8afeed5509c8d6ceeb88be0094e502b85eeb
Oleg <olegmiskoo@gmail.com>
2016-11-05 15:19:10 +0200

    - Created a DatePickerDialog for inbound date and outbound date, which returns correctly (due to ScyScanner requirements) date - A minor changes flights fragment - Added switcher configuration in Other class - Optimized code in FlightsFragments

--- app/src/main/java/progernapplications/scyscannerapplication_v2/config/Other.java
+++ app/src/main/java/progernapplications/scyscannerapplication_v2/config/Other.java
@@ -8,4 +8,22 @@ public class Other
 
     public static int ALERT_COUNTER = 0;
     public static final String SETTINGS_DIALOG_MESSAGE = "If you want to change settings" + "\nthe Language field is REQUIRED!";
+    public static int DATE_SET_CHECKER = 0;
+    public static int FIELD_ADAPTER_CHECKER = 0;
+
+    public static void setDateSetChecker(int dateSetChecker) {
+        DATE_SET_CHECKER = dateSetChecker;
+    }
+
+    public static void setFieldAdapterChecker(int fieldAdapterChecker) {
+        FIELD_ADAPTER_CHECKER = fieldAdapterChecker;
+    }
+
+    public static int getDateSetChecker() {
+        return DATE_SET_CHECKER;
+    }
+
+    public static int getFieldAdapterChecker() {
+        return FIELD_ADAPTER_CHECKER;
+    }
 }
--- app/src/main/java/progernapplications/scyscannerapplication_v2/fragments/FlightsFragment.java
+++ app/src/main/java/progernapplications/scyscannerapplication_v2/fragments/FlightsFragment.java
@@ -1,20 +1,21 @@
 package progernapplications.scyscannerapplication_v2.fragments;
 
-import android.content.DialogInterface;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -31,13 +32,15 @@ import retrofit.Callback;
 import retrofit.Response;
 
 
-public class FlightsFragment extends Fragment implements View.OnClickListener{
+public class FlightsFragment extends Fragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
 
     private AutoCompleteTextView outboundText, inboundText;
     private List<Place> placesList;
     private PlacesAdapter mAdapter;
-    private AutoCompleteTextView outboundDate, inboundDate;
+    private TextView outboundDate, inboundDate;
+    private Switch inboundSwitch;
+
 
     @Nullable
     @Override
@@ -45,26 +48,22 @@ public class FlightsFragment extends Fragment implements View.OnClickListener{
         getActivity().setTitle("Flights booking");
         View myView = inflater.inflate(R.layout.fragment_flights, container, false);
         Button searchButton = (Button) myView.findViewById(R.id.search_button);
-        final Switch inboundSwitch = (Switch) myView.findViewById(R.id.inbound_switch);
-        AutoCompleteTextView outboundDate = (AutoCompleteTextView) myView.findViewById(R.id.outbound_date);
-        final AutoCompleteTextView inboundDate = (AutoCompleteTextView) myView.findViewById(R.id.inbound_date);
-        outboundDate = (AutoCompleteTextView) myView.findViewById(R.id.outbound_date);
+        inboundSwitch = (Switch) myView.findViewById(R.id.inbound_switch);
+        // *****************************************************************
+        inboundDate = (TextView) myView.findViewById(R.id.inbound_date);
+        outboundDate = (TextView) myView.findViewById(R.id.outbound_date);
+        // ******************************************************************
 
-
-
+        // User will change the visibility of inboundDate TextView(by using switch) if he would need this option
         inboundDate.setVisibility(View.INVISIBLE);
 
         outboundText = (AutoCompleteTextView) myView.findViewById(R.id.outbound_text);
         outboundText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
             public void afterTextChanged(Editable editable) {
@@ -75,14 +74,10 @@ public class FlightsFragment extends Fragment implements View.OnClickListener{
         inboundText = (AutoCompleteTextView) myView.findViewById(R.id.inbound_text);
         inboundText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
             public void afterTextChanged(Editable editable) {
@@ -90,17 +85,11 @@ public class FlightsFragment extends Fragment implements View.OnClickListener{
             }
         });
 
-        inboundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (inboundSwitch.isChecked())
-                {
-                    inboundDate.setVisibility(View.VISIBLE);
-                }
-                else inboundDate.setVisibility(View.INVISIBLE);
-            }
-        });
+
+        inboundSwitch.setOnCheckedChangeListener(this);
         searchButton.setOnClickListener(this);
+        outboundDate.setOnClickListener(this);
+        inboundDate.setOnClickListener(this);
 
         return myView;
     }
@@ -109,16 +98,27 @@ public class FlightsFragment extends Fragment implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.search_button:
-                if (!(outboundText.getText().toString().equals(""))) Config.OUTBOUND_LOCATION = getLocationId(outboundText.getText().toString());
-                if (!(inboundText.getText().toString().equals(""))) Config.INBOUND_LOCATION = getLocationId(inboundText.getText().toString());
+                if (!(outboundText.getText().toString().equals("")))
+                    Config.OUTBOUND_LOCATION = getLocationId(outboundText.getText().toString());
+                if (!(inboundText.getText().toString().equals("")))
+                    Config.INBOUND_LOCATION = getLocationId(inboundText.getText().toString());
                 Toast.makeText(getContext(), Config.OUTBOUND_LOCATION + " --- " + Config.INBOUND_LOCATION, Toast.LENGTH_SHORT).show();
-//
+                break;
+            case R.id.outbound_date:
+                Other.setDateSetChecker(0);
+                onCreateDialog().show();
+                break;
+            case R.id.inbound_date:
+                Other.setDateSetChecker(1);
+                onCreateDialog().show();
+                break;
+
+
         }
     }
 
-
     // GET Request to suit the user's input with prediction, using ScyScanner AutoSuggestionPlace API
-    private void getAutoSuggestLocation(String currentQuery) {
+    public void getAutoSuggestLocation(String currentQuery) {
         if (!(outboundText.getText().toString().equals(""))) {
             Call<Places> call = Network.API.getAutoSuggestPlaces(Config.ACCEPT, Config.MARKET_COUNTRY, Config.CURRENCY, Config.LOCALE, currentQuery);
             call.enqueue(new Callback<Places>() {
@@ -141,18 +141,13 @@ public class FlightsFragment extends Fragment implements View.OnClickListener{
         }
     }
 
-
     // Transforms the string, that is suitable for user, to string for ScyScanner API Call. Doesn't changes the UI outbound/inbound string
-    public String getLocationId(String fullString)
-    {
+    public String getLocationId(String fullString) {
         StringBuffer returnString = new StringBuffer();
-        for(int i = 0; i < fullString.length(); i++)
-        {
-            if( fullString.charAt(i) == '(')
-            {
+        for (int i = 0; i < fullString.length(); i++) {
+            if (fullString.charAt(i) == '(') {
                 i++;
-                while(fullString.charAt(i) != ')')
-                {
+                while (fullString.charAt(i) != ')') {
                     returnString.append(fullString.charAt(i));
                     i++;
 
@@ -164,6 +159,36 @@ public class FlightsFragment extends Fragment implements View.OnClickListener{
         return returnString.toString();
     }
 
+    private DatePickerDialog.OnDateSetListener mCallBack = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+            // ***************************************************************************************
+            // This is made, because Android month counter starts from 0, and ScyScanners starts from 1
+            // Plus ScyScanners requests require month formath, if month is < 10 : 01,02,03... etc
+            StringBuffer monthBuf = new StringBuffer();
+            if(i1 >= 10) monthBuf.append((i1+1));
+            else monthBuf.append("0" + (i1+1));
+            // ***************************************************************************************
+
+
+            if (Other.DATE_SET_CHECKER == 0) outboundDate.setText(i + "-" + monthBuf.toString() + "-" + i2);
+            else if (Other.DATE_SET_CHECKER == 1) inboundDate.setText(i + "-" + monthBuf.toString() + "-" + i2);
+        }
+    };
+
+    protected Dialog onCreateDialog() {
+        DatePickerDialog mDateDialog = new DatePickerDialog(getContext(), mCallBack, 2016, 0, 1);
+        return mDateDialog;
+    }
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (inboundSwitch.isChecked()) {
+            inboundDate.setVisibility(View.VISIBLE);
+        } else inboundDate.setVisibility(View.INVISIBLE);
+    }
 
 
 }
